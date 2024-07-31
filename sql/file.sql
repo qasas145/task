14818f9ee802286b0d1c632a48c96b8c9b2936d2
qasas145 <mohamed.sayed14527@yahoo.com>
2024-07-31 14:23:35 +0300

    Sql folder for sql files

---
+++ sql/file.sql
@@ -0,0 +1,17 @@
+-- == inserting users == 
+-- INSERT  INTO Users(Name, Age, Email, [Password]) VALUES('Muhammad Osman', 10, 'moahmed@yahoo.com', 'Osman1020')
+-- INSERT  INTO Users(Name, Age, Email, [Password]) VALUES('Mahmoud Elsayed', 10, 'Elqasas145@gmail.com', 'Hamada1020')
+-- INSERT  INTO Users(Name, Age, Email, [Password]) VALUES('Menna Hamdy', 10, 'Mena145@gmail.com', 'Hamada1020')
+-- INSERT  INTO Users(Name, Age, Email, [Password]) VALUES('Mariem Eleama', 5, 'Mariem@yahoo.com', 'Hamada1020')
+
+-- == inserting posts == 
+
+
+-- insert into Posts(Title, Category, Body,Likes, Shares, UserId) VALUES('Muh post', 'science', 'this the first post of this type', 0, 0,3);
+-- insert into Posts(Title, Category, Body, Likes, Shares,  UserId) VALUES('Ahmed post', 'Maths', 'this the first post of this type', 0, 0,4);
+
+
+
+-- select * from users;
+
+select * from posts;
